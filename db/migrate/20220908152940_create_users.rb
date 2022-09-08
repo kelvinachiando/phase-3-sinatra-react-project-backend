e5aae5dd66c55f95a018022816b7277fb582b608
kelvinachiando <kelvinostin22gmail.com>
2022-09-08 19:39:06 +0300

    commit to main

--- db/migrate/20220908152940_create_users.rb
+++ db/migrate/20220908152940_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :avatar_id
+      t.integer :avatar_id
       t.integer :level_id
       t.timestamps
     end
--- db/migrate/20220908153151_create_questions.rb
+++ db/migrate/20220908153151_create_questions.rb
@@ -2,8 +2,8 @@ class CreateQuestions < ActiveRecord::Migration[6.1]
   def change
     create_table :questions do |t|
       t.integer :level_id
-      t.string :question
-      t.string :answer
+      t.string  :question
+      t.string  :answer 
     end
   end
 end
